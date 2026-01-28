8c800d270e180ecd14e9ade60e50babc228ce568
bole <bole2004@foxmail.com>
2026-01-28 11:47:30 +0800

    Initial Commit

---
+++ Soloist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Soloist
+//
+//  Created by Bole on 2026/1/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Soloist/SoloistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloistApp.swift
+//  Soloist
+//
+//  Created by Bole on 2026/1/28.
+//
+
+import SwiftUI
+
+@main
+struct SoloistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
