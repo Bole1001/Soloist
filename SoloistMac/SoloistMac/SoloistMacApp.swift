347e9a6f8f4d86fdddc02fc85f95d5fc600948c4
bole <bole2004@foxmail.com>
2026-05-09 14:56:38 +0800

    chore: refactor directory to monorepo and init mac workspace

---
+++ SoloistMac/SoloistMac/SoloistMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloistMacApp.swift
+//  SoloistMac
+//
+//  Created by Bole on 2026/5/9.
+//
+
+import SwiftUI
+
+@main
+struct SoloistMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
